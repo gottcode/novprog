bb22ed0b8a1bd8006be2a371c07d2c8f618fcd42
Graeme Gott <graeme@gottcode.org>
2015-01-09 16:26:49 -0500

    Add support for high DPI displays.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 /************************************************************************
  *
- * Copyright (C) 2006, 2007, 2008, 2010, 2012, 2014 Graeme Gott <graeme@gottcode.org>
+ * Copyright (C) 2006, 2007, 2008, 2010, 2012, 2014, 2015 Graeme Gott <graeme@gottcode.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -37,6 +37,7 @@ int main(int argc, char** argv)
 #if !defined(Q_OS_WIN) && !defined(Q_OS_MAC)
 	app.setWindowIcon(QIcon::fromTheme("novprog", QIcon(":/novprog.png")));
 #endif
+	app.setAttribute(Qt::AA_UseHighDpiPixmaps, true);
 
 	LocaleDialog::loadTranslator("novprog_");
 

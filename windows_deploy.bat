08ac8c0ca85f19dd2e49b6cb7ea292a17d7fcd67
Graeme Gott <graeme@gottcode.org>
2018-05-24 13:30:33 -0400

    Improve Windows deployment.

--- windows_deploy.bat
+++ windows_deploy.bat
@@ -1,3 +1,5 @@
+@ECHO ON>windows\dirs.nsh
+@ECHO ON>windows\files.nsh
 @ECHO OFF
 
 SET APP=NovProg
@@ -5,9 +7,6 @@ SET VERSION=3.1.3
 
 ECHO Copying executable
 MKDIR %APP%
-TYPE COPYING | FIND "" /V > %APP%\COPYING.txt
-TYPE CREDITS | FIND "" /V > %APP%\CREDITS.txt
-TYPE README | FIND "" /V > %APP%\README.txt
 COPY release\%APP%.exe %APP% >nul
 strip %APP%\%APP%.exe
 
@@ -15,21 +14,48 @@ ECHO Copying translations
 SET TRANSLATIONS=%APP%\translations
 MKDIR %TRANSLATIONS%
 COPY translations\*.qm %TRANSLATIONS% >nul
-COPY %QTDIR%\translations\qtbase_*.qm %TRANSLATIONS% >nul
 
-ECHO Copying Qt libraries
-COPY %QTDIR%\bin\libgcc_s_dw2-1.dll %APP% >nul
-COPY "%QTDIR%\bin\libstdc++-6.dll" %APP% >nul
-COPY %QTDIR%\bin\libwinpthread-1.dll %APP% >nul
-COPY %QTDIR%\bin\Qt5Core.dll %APP% >nul
-COPY %QTDIR%\bin\Qt5Gui.dll %APP% >nul
-COPY %QTDIR%\bin\Qt5Widgets.dll %APP% >nul
+ECHO Copying Qt
+%QTDIR%\bin\windeployqt.exe --verbose 0 --release --no-angle --no-opengl-sw --no-svg %APP%\%APP%.exe
+RMDIR /S /Q %APP%\imageformats
 
-MKDIR %APP%\platforms
-COPY %QTDIR%\plugins\platforms\qwindows.dll %APP%\platforms >nul
+ECHO Creating ReadMe
+TYPE README >> %APP%\ReadMe.txt
+ECHO. >> %APP%\ReadMe.txt
+ECHO. >> %APP%\ReadMe.txt
+ECHO CREDITS >> %APP%\ReadMe.txt
+ECHO ======= >> %APP%\ReadMe.txt
+ECHO. >> %APP%\ReadMe.txt
+TYPE CREDITS >> %APP%\ReadMe.txt
+ECHO. >> %APP%\ReadMe.txt
+ECHO. >> %APP%\ReadMe.txt
+ECHO NEWS >> %APP%\ReadMe.txt
+ECHO ==== >> %APP%\ReadMe.txt
+ECHO. >> %APP%\ReadMe.txt
+TYPE NEWS >> %APP%\ReadMe.txt
+
+ECHO Creating installer
+CD %APP%
+SETLOCAL EnableDelayedExpansion
+SET "parentfolder=%__CD__%"
+FOR /R . %%F IN (*) DO (
+  SET "var=%%F"
+  ECHO Delete "$INSTDIR\!var:%parentfolder%=!" >> ..\windows\files.nsh
+)
+FOR /R /D %%F IN (*) DO (
+  TYPE ..\windows\dirs.nsh > temp.txt
+  SET "var=%%F"
+  ECHO RMDir "$INSTDIR\!var:%parentfolder%=!" > ..\windows\dirs.nsh
+  TYPE temp.txt >> ..\windows\dirs.nsh
+)
+DEL temp.txt
+ENDLOCAL
+CD ..
+makensis.exe /V0 windows\installer.nsi
 
 ECHO Making portable
 MKDIR %APP%\Data
+COPY COPYING %APP%\COPYING.txt >nul
 
 ECHO Creating compressed file
 CD %APP%
@@ -39,3 +65,5 @@ MOVE %APP%\%APP%_%VERSION%.zip . >nul
 
 ECHO Cleaning up
 RMDIR /S /Q %APP%
+DEL windows\dirs.nsh
+DEL windows\files.nsh

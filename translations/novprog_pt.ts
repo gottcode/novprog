5732601d32dd0bf4a8657c96d96254e29e52ead3
Graeme Gott <graeme@gottcode.org>
2018-05-27 08:00:50 -0400

    Sync translations with Transifex.

---
+++ translations/novprog_pt.ts
@@ -0,0 +1,155 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt">
+<context>
+    <name>Bar</name>
+    <message numerus="yes">
+        <source>%Ln word(s)</source>
+        <translation>
+            <numerusform>%Ln palavra</numerusform>
+            <numerusform>%Ln palavras</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>LocaleDialog</name>
+    <message>
+        <source>Select application language:</source>
+        <translation>Selecione o idioma da aplicação:</translation>
+    </message>
+    <message>
+        <source>&lt;System Language&gt;</source>
+        <translation>&lt;Idioma do Sistema&gt;</translation>
+    </message>
+    <message>
+        <source>Note</source>
+        <translation>Nota</translation>
+    </message>
+    <message>
+        <source>Please restart this application for the change in language to take effect.</source>
+        <translation>Por favor reinicie esta aplicação para que a alteração do idioma surta efeito.</translation>
+    </message>
+</context>
+<context>
+    <name>NovelDialog</name>
+    <message>
+        <source>Name:</source>
+        <translation>Nome:</translation>
+    </message>
+    <message>
+        <source>Goals</source>
+        <translation>Objetivos</translation>
+    </message>
+    <message>
+        <source>N/A</source>
+        <translation>N/A</translation>
+    </message>
+    <message>
+        <source>Date Range</source>
+        <translation>Intervalo de Datas</translation>
+    </message>
+    <message>
+        <source>Word Count</source>
+        <translation>Contagem de Palavras:</translation>
+    </message>
+    <message>
+        <source>Total:</source>
+        <translation>Total:</translation>
+    </message>
+    <message>
+        <source>Daily:</source>
+        <translation>Diário:</translation>
+    </message>
+    <message>
+        <source>Start On:</source>
+        <translation>Começar a:</translation>
+    </message>
+    <message>
+        <source>End On:</source>
+        <translation>Terminar a:</translation>
+    </message>
+    <message>
+        <source>Start At:</source>
+        <translation>Começar a:</translation>
+    </message>
+    <message>
+        <source>Add Novel</source>
+        <translation>Adicionar Novela</translation>
+    </message>
+    <message>
+        <source>Edit Novel</source>
+        <translation>Editar Novela</translation>
+    </message>
+    <message>
+        <source>Sorry</source>
+        <translation>Desculpe</translation>
+    </message>
+    <message>
+        <source>A novel already exists with that name.</source>
+        <translation>Já existe uma novela com esse nome.</translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <source>Novel Progress</source>
+        <translation>Progresso da Novela</translation>
+    </message>
+    <message>
+        <source>Total</source>
+        <translation>Total</translation>
+    </message>
+    <message>
+        <source>Daily</source>
+        <translation>Diário</translation>
+    </message>
+    <message>
+        <source>Word count:</source>
+        <translation>Contagem de palavras:</translation>
+    </message>
+    <message>
+        <source>&amp;Novel</source>
+        <translation>&amp;Novela</translation>
+    </message>
+    <message>
+        <source>&amp;New Novel</source>
+        <translation>&amp;Nova Novela</translation>
+    </message>
+    <message>
+        <source>&amp;Edit Novel</source>
+        <translation>&amp;Editar Novela</translation>
+    </message>
+    <message>
+        <source>&amp;Delete Novel</source>
+        <translation>&amp;Eliminar Novela</translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation>&amp;Sair</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation>&amp;Novo</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation>&amp;Editar</translation>
+    </message>
+    <message>
+        <source>&amp;Delete</source>
+        <translation>Elimina&amp;r</translation>
+    </message>
+    <message>
+        <source>Delete novel?</source>
+        <translation>Eliminar a novela?</translation>
+    </message>
+    <message>
+        <source>Delete progress for the novel &apos;%1&apos;?</source>
+        <translation>Eliminar o progresso da novela &quot;%1&quot;?</translation>
+    </message>
+    <message>
+        <source>This action cannot be undone.</source>
+        <translation>Esta ação não pode ser desfeita.</translation>
+    </message>
+</context>
+</TS>

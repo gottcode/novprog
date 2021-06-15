c61d2b8fa662082dab71af025d0b802b4b150deb
Graeme Gott <graeme@gottcode.org>
2021-06-15 13:05:32 -0400

    Sync translations with Transifex

---
+++ translations/novprog_uk.ts
@@ -0,0 +1,156 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uk">
+<context>
+    <name>Bar</name>
+    <message numerus="yes">
+        <source>%Ln word(s)</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>LocaleDialog</name>
+    <message>
+        <source>Select application language:</source>
+        <translation>Виберіть мову програми:</translation>
+    </message>
+    <message>
+        <source>&lt;System Language&gt;</source>
+        <translation>&lt;System Language&gt;</translation>
+    </message>
+    <message>
+        <source>Note</source>
+        <translation>Нотатка</translation>
+    </message>
+    <message>
+        <source>Please restart this application for the change in language to take effect.</source>
+        <translation>Будь ласка, перезапустіть цю програму, щоб зміна мови набрала чинності.</translation>
+    </message>
+</context>
+<context>
+    <name>NovelDialog</name>
+    <message>
+        <source>Name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Goals</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>N/A</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Date Range</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Word Count</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Total:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Daily:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Start On:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>End On:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Start At:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Add Novel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Edit Novel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sorry</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>A novel already exists with that name.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <source>Novel Progress</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Total</source>
+        <translation>Всього</translation>
+    </message>
+    <message>
+        <source>Daily</source>
+        <translation>Щодня</translation>
+    </message>
+    <message>
+        <source>Word count:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Novel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;New Novel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Edit Novel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Delete Novel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation>&amp;Вийти</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation>&amp;Новий</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation>&amp;Редагувати</translation>
+    </message>
+    <message>
+        <source>&amp;Delete</source>
+        <translation>&amp;Видалити</translation>
+    </message>
+    <message>
+        <source>Delete novel?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Delete progress for the novel &apos;%1&apos;?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This action cannot be undone.</source>
+        <translation>Цю дію не можна скасувати.</translation>
+    </message>
+</context>
+</TS>

e5ce1bd25f8d8adcedcf8eff64f7643d9366a2f3
Graeme Gott <graeme@gottcode.org>
2026-01-16 11:35:10 -0500

    Standardize copyright statements

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 /*
-	SPDX-FileCopyrightText: 2006-2022 Graeme Gott <graeme@gottcode.org>
+	SPDX-FileCopyrightText: 2006 Graeme Gott <graeme@gottcode.org>
 
 	SPDX-License-Identifier: GPL-3.0-or-later
 */

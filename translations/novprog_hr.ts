44d2373b935c22d3ec274d27aabfdf1a692c2cdd
Milo Ivir <43657314+milotype@users.noreply.github.com>
2026-03-30 13:09:57 +0200

    Add Croatian translation (#3)

---
+++ translations/novprog_hr.ts
@@ -0,0 +1,171 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hr" sourcelanguage="en">
+<context>
+    <name>Bar</name>
+    <message numerus="yes">
+        <source>%Ln word(s)</source>
+        <translation>
+        	<numerusform>%Ln riječ</numerusform>
+        	<numerusform>%Ln riječi</numerusform>
+        	<numerusform>%Ln riječi</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>LocaleDialog</name>
+    <message>
+        <source>Select application language:</source>
+        <translation>Odaberi jezik aplikacije:</translation>
+    </message>
+    <message>
+        <source>&lt;System Language&gt;</source>
+        <translation>&lt;Jezik sustava&gt;</translation>
+    </message>
+    <message>
+        <source>Note</source>
+        <translation>Bilješka</translation>
+    </message>
+    <message>
+        <source>Please restart this application for the change in language to take effect.</source>
+        <translation>Ponovo pokreni ovu aplikaciju kako bi promjena jezika stupila na snagu.</translation>
+    </message>
+</context>
+<context>
+    <name>NovelDialog</name>
+    <message>
+        <source>Name:</source>
+        <translation>Ime:</translation>
+    </message>
+    <message>
+        <source>Goals</source>
+        <translation>Ciljevi</translation>
+    </message>
+    <message>
+        <source>N/A</source>
+        <translation>--</translation>
+    </message>
+    <message>
+        <source>Date Range</source>
+        <translation>Raspon datuma</translation>
+    </message>
+    <message>
+        <source>Word Count</source>
+        <translation>Zbroj riječi</translation>
+    </message>
+    <message>
+        <source>Total:</source>
+        <translation>Ukupno:</translation>
+    </message>
+    <message>
+        <source>Daily:</source>
+        <translation>Dnevno:</translation>
+    </message>
+    <message>
+        <source>Start On:</source>
+        <translation>Početak:</translation>
+    </message>
+    <message>
+        <source>End On:</source>
+        <translation>Kraj</translation>
+    </message>
+    <message>
+        <source>Start At:</source>
+        <translation>Započni pri:</translation>
+    </message>
+    <message>
+        <source>Add Novel</source>
+        <translation>Dodaj roman</translation>
+    </message>
+    <message>
+        <source>Edit Novel</source>
+        <translation>Uredi roman</translation>
+    </message>
+    <message>
+        <source>Sorry</source>
+        <translation>Oprosti</translation>
+    </message>
+    <message>
+        <source>A novel already exists with that name.</source>
+        <translation>Roman s tim imenom već postoji.</translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <source>Novel Progress</source>
+        <translation>Napredak romana</translation>
+    </message>
+    <message>
+        <source>Total</source>
+        <translation>Ukupno</translation>
+    </message>
+    <message>
+        <source>Daily</source>
+        <translation>Dnevno</translation>
+    </message>
+    <message>
+        <source>Word count:</source>
+        <translation>Zbroj riječi:</translation>
+    </message>
+    <message>
+        <source>&amp;New Novel</source>
+        <translation>&amp; Novi roman</translation>
+    </message>
+    <message>
+        <source>&amp;Edit Novel</source>
+        <translation>&amp;Uredi roman</translation>
+    </message>
+    <message>
+        <source>&amp;Delete Novel</source>
+        <translation>&amp;Izbriši roman</translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation>&amp;Zatvori aplikaciju</translation>
+    </message>
+    <message>
+        <source>&amp;Novel</source>
+        <translation>&amp;Roman</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation>&amp;Nova</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation>&amp;Uredi</translation>
+    </message>
+    <message>
+        <source>&amp;Delete</source>
+        <translation>&amp;Izbriši</translation>
+    </message>
+    <message>
+        <source>Delete novel?</source>
+        <translation>Izbrisati roman?</translation>
+    </message>
+    <message>
+        <source>Delete progress for the novel '%1'?</source>
+        <translation>Izbrisati napredak za roman „%1”?</translation>
+    </message>
+    <message>
+        <source>This action cannot be undone.</source>
+        <translation>Ova je nepovratna radnja.</translation>
+    </message>
+    <message>
+        <source>Set words written today</source>
+        <translation>Postavi broj danas napisanih riječi</translation>
+    </message>
+    <message>
+        <source>Words written today:</source>
+        <translation>Broj danas napisanih riječi:</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <source>A wordcount graphing program</source>
+        <translation>Aplikacija za grafički prikaz broja riječi</translation>
+    </message>
+</context>
+</TS>
